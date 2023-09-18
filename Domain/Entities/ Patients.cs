78fbf40826de4fdae6e2636d7ae7d958d5a2b6b1
giovannysocas <giovannysocas@gmail.com>
2023-09-18 16:01:59 -0500

    second commit

---
+++ Domain/Entities/ Patients.cs
@@ -0,0 +1,15 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+
+    public class Patients : BaseEntity 
+    {
+        public string PatientName {get;set;}
+        public string PhoneNumber {get;set;}
+        
+    }
+
